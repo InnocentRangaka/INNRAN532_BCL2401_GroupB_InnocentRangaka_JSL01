0af9c233e08b7a5275b7798b1824b41c8174fd21
Innocent Rangaka <itrangaka@gmail.com>
2024-03-12 11:45:04 +0200

    finalize JSL01

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# INNRAN532_BCL2401_GroupB_InnocentRangaka_JSL01
+### INNRAN532_BCL2401_GroupB_InnocentRangaka_JSL01
+
+# Syntax Validation for Pet Input
+
+Providing basic syntax validation for user-entered pet code in "pet_<YEAR><PET_NAME>" format using regular expressions to validate different parts of the pet code format. Based on combined prefix, year, and pet name validations, suitable text messages and CSS classes get assigned for the overall outcome.
+
+![Syntax Validation Result](jsl_01_final_result.gif)
+
+## Functionality
+
+* Retrieves user input from an element with the ID petInput;
+* Checks if the input follows a specific format likely resembling "pet_<YEAR><PET_NAME>";
+* Validates the presence of the prefix "pet_";
+* Validates the year format (starting with 19 or 20 followed by two digits between 1900 and 2099);
+* Validates the pet name format (starting with an uppercase letter, containing only letters);
+* Provides informative feedback through a displayed message ("Valid Syntax" or "Invalid Syntax");
+* Updates the validation status visually using CSS classes ("valid" or "invalid");
+
+## Usage
+
+* Enter the pet code on an input element with the ID petInput.
+* Click "validate" button to validate the entered pet code.
+* The validation message will be displayed on an element with the ID result below the input and button elements.
+
+## Technologies
+
+* HTML
+* CSS
+* JS
+* VS Code
--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById('petInput').value,
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
-    let error = '';
-    let errorStatus = 'valid';
+    result = '', // Placeholder for validation result
+    statusClass = '', // Placeholder for validation status
+    removeClass = '', // Placeholder for class to be removed
+    resultElement = document.getElementById('result'); // Element for displaying validation result
 
     // TODO: Write your validation logic here
     // Check if input starts with 'pet_' and followed by alphanumeric characters
-    if(input.length === 0){return;}
 
-    const prefixReg = /pet_/,
-    yearRegex = /^(19|20)[\d]{2,2}$/,
-    nameRegex = /^[A-Z]+[a-zA-Z]+$/;
+    const prefixRegex = /pet_/, // Regex to match the literal string "pet_" at the beginning of the input.
+    yearRegex = /^(19|20)[\d]{2,2}$/, // Regex to match a valid year format between 1900 and 2099.
+    nameRegex = /^[A-Z]+[a-zA-Z]+$/; // Regex to match a pet name format starting with an uppercase letter and contains only letters.
 
-    const petValue = input.substr(0, 4),
-    yearValue = input.substr(4, 4),
-    nameValue = input.substr(8);
+    const petValue = input.substr(0, 4), // Retrieves first 4 characters from petInput element.
+    yearValue = input.substr(4, 4), // Retrieves second 4 characters from petInput element.
+    nameValue = input.substr(8);  // Retrieves the last characters after second 4 characters from petInput element.
 
-    const codeSyntax = prefixReg.test(petValue) && yearRegex.test(yearValue) && nameRegex.test(nameValue);
+    // Validates the presence of the prefix "pet_".
+    // Validates the year format (starting with 19 or 20 followed by two digits between 00 and 99).
+    // Validates the pet name format (starting with an uppercase letter, containing only letters).
+    // Checks if the input follows a specific "pet_<YEAR><PET_NAME>" format.
+    const codeSyntax = prefixRegex.test(petValue) && yearRegex.test(yearValue) && nameRegex.test(nameValue); 
 
-    if(!codeSyntax){
-        if(!prefixReg.test(petValue)){
-            error = `Code must start with "pet_".`;
-        }
-        else if(!yearRegex.test(yearValue)) {
-            error = `"${yearValue}" must be a year`;
-        }
-        else if(!nameRegex.test(nameValue)){
-            error = `${nameValue} must be a Pet's Name with capitalized "${nameValue.charAt(0)}"`;
-        }
-        errorStatus = "invalid";
-    }
+    // Validation result and the classes assignment based on the input and validation status
+    // Checks if the codeSyntax variable is false or the user input length is zero (empty input)
+    result = (!codeSyntax || input.length === 0)? 'Invalid Syntax' : 'Valid Syntax'; // If true, set validation result to "Invalid Syntax", otherwise set it to "Valid Syntax".
+    statusClass = (!codeSyntax || input.length === 0)? 'invalid' : 'valid'; // If true, set validation class to "invalid", otherwise set it to "valid".
+    removeClass = (!codeSyntax || input.length === 0)? 'valid' : 'invalid'; // If true, set class to be removed to "valid", otherwise set it to "invalid".
 
-    result = (error !== '')? `${error} 🔴` : `${input} 🟢`;
-
-    document.getElementById('result').innerText = result;
+    resultElement.innerText = result; // Displays the final validation result (valid or invalid)
+    resultElement.classList.remove(removeClass); // Removes existing valid or invalid class
+    resultElement.classList.add(statusClass); // Add a valid or invalid class
 }
 
 
